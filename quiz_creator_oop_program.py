2b6a72afda3793b61db2465bde33471c7e863c71
Chris <chriswell.mangahas@gmail.com>
2025-05-31 23:53:13 +0800

    Added code for pseudocode 1.

--- quiz_creator_oop_program.py
+++ quiz_creator_oop_program.py
@@ -1,6 +1,12 @@
 import json
 
 #1. define class that represents a single quiz question
+class Question:
+    def __init__(self, prompt, choices, correct_answer):
+        self.prompt = prompt
+        self.choices = choices
+        self.correct_answer = correct_answer
+
 #2. convert the question to a dictionary for json
 #3. define class that manages the quiz creation process
 #4. initialize a storage for quiz data and the output filename
