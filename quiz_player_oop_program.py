3510c6a81c33ee80aac9e7e13321ba2c63d908e4
Chris <chriswell.mangahas@gmail.com>
2025-06-01 10:46:04 +0800

    Added code for pseudocode 1: initializing colorama, quiz questions, and score.

--- quiz_player_oop_program.py
+++ quiz_player_oop_program.py
@@ -3,6 +3,23 @@ import random
 from colorama import init, Fore, Style
 
 #1. initialize colorama
+init(autoreset=True)
+
+class QuizQuestion:
+    def __init__(self, prompt, choices, correct_answer):
+        self.prompt = prompt
+        self.choices = choices
+        self.correct_answer = correct_answer
+
+    def from_dict(cls, data):
+        return cls(data["question"], data["choices"], data["correct_answer"])
+
+class QuizPlayer:
+    def __init__(self, filename="quiz_questions.json"):
+        self.filename = filename
+        self.questions = []
+        self.score = 0
+
 #2. load questions from quiz_questions.json
 #3. display welcome message
 #4. shuffle the questions and start the quiz
